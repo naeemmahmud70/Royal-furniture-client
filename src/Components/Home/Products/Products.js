e99d933ac3e4c1074d8c092951d79b7530b774ae
Naeem Mahmud Al-Amin <naeemmahmud370@gmail.com>
2021-08-03 15:03:09 +0600

    products card added

--- src/Components/Home/Products/Products.js
+++ src/Components/Home/Products/Products.js
@@ -1,17 +1,26 @@
 import React, { useEffect, useState } from 'react';
+import Product from './Product/Product';
+import './Products.css'
 
 const Products = () => {
     const [products, setProducts] = useState([])
-    console.log(products)
 
-    useEffect(()=>{
+    useEffect(() => {
         fetch('http://localhost:5000/products')
-        .then(res=> res.json())
-        .then(data => setProducts(data))
+            .then(res => res.json())
+            .then(data => setProducts(data))
     })
     return (
         <div>
-            <h1>products {products.length}</h1>
+            <div className="text-center m-3">
+                <h2>Popular Products</h2>
+                <h5>Checkout our latest collections.</h5>
+            </div>
+            <div className="card-flex">
+                {
+                    products.map(product => <Product product={product} key={product.name}></Product>)
+                }
+            </div>
         </div>
     );
 };
